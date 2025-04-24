3e62a93c46c0008b3f75ac9e055681d5c19086b9
soboldsantosh <santosh@soboldltd.com>
2025-04-24 10:36:53 +0100

    Fix Swagger UI to load openapi.yaml and hide Explore bar

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,6 +4,7 @@ window.onload = function () {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "openapi.yaml",
+    configUrl: null,
     dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
